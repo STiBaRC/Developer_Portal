fbf44a4291be1c55d5180b678314c7ae4face498
Ben Border <ben@bunnbuns.net>
2021-07-28 16:39:49 -0400

    show signed in state on index

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
 if(loggedIn) {
-    window.location = "/applications.html";
+    // window.location = "/applications.html";
+    $("topBtn").textContent = "My Applications";
+    $("topBtn").href = "./applications.html";
+    $("pfpBtn").style.display = "";
 }
