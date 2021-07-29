7cab1b2221e413ddd6c28d38f7a57670cd977d57
Ben Border <ben@bunnbuns.net>
2021-07-29 15:15:55 -0400

    new app dialog box

--- assets/js/applications.js
+++ assets/js/applications.js
@@ -1,13 +1,11 @@
-window.onload = function() {
-	var sess = window.localStorage.getItem("sess");
-	var xhr = new XMLHttpRequest();
-	xhr.open("post","api/getapps.sjs", true);
-	xhr.send("sess="+sess);
-	xhr.onload = function(e) {
-		// var tmp = xhr.responseText.split("\n");
-		// document.getElementById("applist").innerHTML = "";
-		// for (i = 0; i < tmp.length -1; i++) {
-		// 	toLink(tmp[i]);
-		// }
+function newApp() {
+	$("newAppModal").style.display = "";
+	$("newAppName").focus();
+}
+
+function closeDialogs() {
+	const overlays = $(".overlay");
+	for (var i = 0; i < overlays.length; i++) {
+		overlays[i].style.display = "none";
 	}
 }
